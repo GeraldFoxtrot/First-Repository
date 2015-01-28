b14161efd8d93937d27995656733d9387b20b9e2
Ryan Baxter <rcbaxter42@students.tntech.edu>
2015-01-28 09:29:44 -0600

    Made WriteFile.h a class

---
+++ ThoseFiles/WriteFile.h
@@ -0,0 +1,20 @@
+#if !defined WRITE_FILE
+#define WRITE_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
+class WriteFile
+{
+   ofstream output_file;
+   bool closed;
+}write;
+
+WriteFile* createWriteFile(const char* file_name);
+void destroyWriteFile(WriteFile* wf);
+void writeLine(WriteFile* wf, String* line);
+void close(WriteFile* wf);
+
+#endif
